be0ef5a120e8f16df10fa026d12dd6263564ba0b
etisnow <freedomax@mail.ru>
2024-08-18 21:41:49 +0300

    client jino adress

--- src/client/utils/requests.tsx
+++ src/client/utils/requests.tsx
@@ -2,9 +2,9 @@ import axios from "axios";
 import EventSource from "eventsource";
 import { clearError, clearRoomValidationError, RoomValidationStatus, setError, setLoadedName, setMyName, setPlayerData, setRoomData, setRoomLink, setRoomValidation, setRoomValidationError, store } from "../store/store";
 
-const NGROK_URL = 'https://6175-176-115-145-250.ngrok-free.app'
+const JINO_URL = 'https://6175-176-115-145-250.ngrok-free.app'
 
-axios.defaults.baseURL = window.location.href.includes('localhost') ? 'http://localhost:3000' : NGROK_URL
+axios.defaults.baseURL = window.location.href.includes('localhost') ? 'http://ba5699d52128.vps.myjino.ru:49294' : JINO_URL
 axios.defaults.headers.common['Authorization'] = localStorage.getItem('authKey')
 axios.defaults.headers.common['ngrok-skip-browser-warning'] = 'true'
 
